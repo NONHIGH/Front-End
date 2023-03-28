78947b433e231d21248b1ef6ddfa077fb0e5b8e9
NONHIGH <angelones_24@hotmail.com>
2023-03-28 17:56:07 -0300

    Incorporacion de componente about-me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,39 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      percent:80,
+      radius:40,
+      title:'Habilidad Comunicativa',
+      titleFontSize:'5',
+      titleFontWeight:'50',
+      outerStrokeWidth:5,
+      innerStrokeWidth:2,
+      outerStrokeColor:'#ffffff',
+      innerStrokeColor:'#000000',
+      animation:true,
+      animationDuration:1000,
+      lazy:true,
+      responsive:true,
+      showUnits:false,
+      showSubtitle:false,
+      imageHeight:40,
+      imageWidth:40
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
