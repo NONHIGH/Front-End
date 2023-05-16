49da1fe9df6140598e3368548b1938d20210ba17
NONHIGH <angelones_24@hotmail.com>
2023-05-15 22:14:04 -0300

    hs

--- src/environment/environments.ts
+++ src/environment/environments.ts
@@ -1,4 +1,5 @@
 export const environment={
-    api:"https://backend-ppjr.onrender.com/api",
+    //api:"https://backend-ppjr.onrender.com/api",
+    api:"http://localhost:8080/api",
     production:true
 }
